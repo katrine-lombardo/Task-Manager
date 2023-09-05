c8871d6f1d81272dfb77d4275ae913c6199003f3
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-09-05 17:17:38 -0400

    Feature 7 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from accounts.forms import SignupForm, LoginForm
 
-# Create your views here.
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            password_confirmation = form.cleaned_data['password_confirmation']
+            if password == password_confirmation:
+                user = User.objects.create_user(
+                    username,
+                    password=password,
+                )
+                login(request, user)
+                return redirect("home")
+            else:
+                form.add_error("password", "the passwords do not match")
+    else:
+        form = SignupForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(
+                request,
+                username=username,
+                password=password,
+                )
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = LoginForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/login.html", context)
