8948563439dc3514045184f81a6b3c6b31f90c34
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-09-05 17:07:13 -0400

    Feature 6 complete

--- projects/views.py
+++ projects/views.py
@@ -7,6 +7,6 @@ from projects.models import Project
 def list_projects(request):
     projects = Project.objects.all()
     context = {
-        "list_project": projects,
+        "list_projects": projects,
     }
     return render(request, "projects/list_projects.html", context)
